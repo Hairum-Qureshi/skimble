d5c6463e1005cc077c09094f4e728b1acb7f6f75
Hairum Qureshi <hqureshi@udel.edu>
2026-04-24 02:18:49 -0400

    resolved issues with chrome extension

--- skimble/src/script.ts
+++ skimble/src/script.ts
@@ -1,3 +1,2 @@
-document.querySelector("button")?.addEventListener('click', () => {
-    alert("PRESSED!")
-});
+const tags = Array.from(document.getElementsByTagName("*")).map(el => el.tagName);
+console.log('from extension', tags);
--- skimble/vite.config.ts
+++ skimble/vite.config.ts
@@ -2,6 +2,20 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+   build: {
+    rollupOptions: {
+      input: {
+        main: "src/script.ts",
+        popup: "index.html"
+      },
+      output: {
+        entryFileNames: (chunk) => {
+          if (chunk.name === "script") return "script.js";
+          return "[name].js";
+        }
+      }
+    }
+  },
   plugins: [
     tailwindcss(),
   ],
